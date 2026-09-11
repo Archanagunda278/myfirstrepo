ce6aa48d64efaa39284145f5c1945b527e53bc41
archana <archana@gmail.com>
2026-09-11 10:10:41 +0000

    java filedataadded

---
+++ app.java
@@ -0,0 +1,7 @@
+// Save this file exactly as "HelloWorld.java"
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints the text to the console
+        System.out.println("Hello, World!");
+    }
+}
